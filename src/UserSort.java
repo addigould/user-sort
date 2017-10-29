97ac74794de2fdf2b86f79dc51e826f8d0e1be35
Addison Gould <gouldadd@grinnell.edu>
2017-10-29 14:19:39 -0500

    starting to add file writing functionality

--- src/UserSort.java
+++ src/UserSort.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -12,7 +15,7 @@ public class UserSort {
      static String[] outputList = null;
      static int outputListSize = 0;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 	
        
         
@@ -68,7 +71,16 @@ public class UserSort {
 		for(int i=1; i<listSize; i++){
 			insert(initialList[i]);
 		}	
+		//print the final ordering to the console
 		System.out.println(Arrays.toString(outputList));
+		//write the ordering to a text file
+		//TODO: generate filenames based on date/time so as not to overwrite previous outputs
+		PrintWriter writer = new PrintWriter("outputOrder.txt", "UTF-8");
+		writer.println("Final list ordering");
+		for(int j = 0; j < outputListSize; j++){
+			writer.println("Item " + (j + 1) + ": " + outputList[j]);
+		}
+		writer.close();
 	}
 	
 
